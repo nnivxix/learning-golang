42a28fad28b985d6037e8346a80bbff2d735c743
nnivxix <adhe.hanasa@gmail.com>
2025-10-28 16:45:31 +0700

    learn: boolean

---
+++ boolean.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var isLoggedIn bool = true
+	var isPremiumUser bool = false
+
+	fmt.Println("Is Logged In:", isLoggedIn)
+	fmt.Println("Is Premium User:", isPremiumUser)
+}
